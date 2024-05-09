15227885d7fa1ef733df56e3be8f40776c7364a0
Artem <krutog727@gmail.com>
2024-05-10 00:11:45 +0300

    Added about me and code pages

--- files/index/script.js
+++ files/index/script.js
@@ -1,21 +1,3 @@
-const dsName = "bumer32"
-
-function showToast(message) {
-    const toast = document.createElement('div');
-    toast.classList.add('toast');
-    toast.textContent = message;
-    document.body.appendChild(toast);
-
-    setTimeout(() => {
-        document.body.removeChild(toast);
-    }, 3000);
-}
-
-function ds() {
-    navigator.clipboard.writeText(dsName)
-    showToast("Copied to clipboard")
-}
-
 const eyeLeft = document.getElementById("eyeLeft")
 const eyeRight = document.getElementById("eyeRight")
 const ballLeft = document.getElementById("ballLeft")
